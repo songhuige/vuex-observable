e15ef0148824b503ede8e20316b6d8d9d8b4cb9c
suhayl <itessay@163.com>
2020-05-07 01:00:39 +0800

    Upgrade rxjs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { Subject } from 'rxjs/Subject'
-import { ActionsObservable } from './ActionsObservable'
 
-export function observableAction (init) {
-  const input$ = new Subject()
-  const action$ = new ActionsObservable(input$)
+import { Subject } from 'rxjs'
+
+export function observableAction(init) {
+  const input$ = new Subject();
+  const action$ = input$.asObservable();
 
   let $output
-  return function observableAction (context, payload) {
+  return function observableAction(context, payload) {
     if (!$output) {
       $output = init(action$, context)
     }
